c1216715371e7828814dbf8b758bf52634cf6e61
ngocdiep221105-tech <ngocdiep221105@gmail.com>
2025-11-03 19:21:03 +0100

    Add files via upload

---
+++ Equipement.java
@@ -0,0 +1,21 @@
+package objets;
+
+public enum Equipement {
+	CASQUE("casque"),
+    BOUCLIER("bouclier");
+
+    private String nom;
+
+    private Equipement(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
+}
